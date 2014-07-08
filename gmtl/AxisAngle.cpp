4b780f523c5f752727ee7bdf0544d37c7878369a
Chad Austin <chad@imvu.com>
2014-07-08 14:23:57 -0700

    oops, forgot these files

---
+++ gmtl/AxisAngle.cpp
@@ -0,0 +1,6 @@
+#include <gmtl/AxisAngle.h>
+
+namespace gmtl {
+    const AxisAnglef AXISANGLE_IDENTITYF( 0.0f, 1.0f, 0.0f, 0.0f );
+    const AxisAngled AXISANGLE_IDENTITYD( 0.0, 1.0, 0.0, 0.0 );
+}
---
+++ gmtl/EulerAngle.cpp
@@ -0,0 +1,10 @@
+#include <gmtl/EulerAngle.h>
+
+namespace gmtl {
+    const EulerAngle<float, XYZ> EULERANGLE_IDENTITY_XYZF( 0.0f, 0.0f, 0.0f );
+    const EulerAngle<double, XYZ> EULERANGLE_IDENTITY_XYZD( 0.0, 0.0, 0.0 );
+    const EulerAngle<float, ZYX> EULERANGLE_IDENTITY_ZYXF( 0.0f, 0.0f, 0.0f );
+    const EulerAngle<double, ZYX> EULERANGLE_IDENTITY_ZYXD( 0.0, 0.0, 0.0 );
+    const EulerAngle<float, ZXY> EULERANGLE_IDENTITY_ZXYF( 0.0f, 0.0f, 0.0f );
+    const EulerAngle<double, ZXY> EULERANGLE_IDENTITY_ZXYD( 0.0, 0.0, 0.0 );
+}
